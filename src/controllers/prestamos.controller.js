888764bae472e78a113419910cfab54b4f98815c
LichiLandaburu <lichilanda@gmail.com>
2022-11-16 01:04:38 -0300

    Agregado prestmaos

--- src/controllers/prestamos.controller.js
+++ src/controllers/prestamos.controller.js
@@ -5,10 +5,10 @@ export const createPrestamo = async (req, res) => {
         const { nro_afiliado, monto, cant_cuotas, interes } = req.body;
         if (!nro_afiliado || !monto || !cant_cuotas || !interes) return res.status(400).json({ error: "Faltan campos obligatorios" });
         
-        const prestamos = getConnection().data.afiliados;
+        const prestamos = getConnection().data.prestamos;
                 
-        const fecha = new Date().toLocaleDateString();
-        const nuevoPrestamo = { ...req.body, fecha };
+        const createdAt = new Date().toLocaleDateString();
+        const nuevoPrestamo = { ...req.body, createdAt };
         prestamos.push(nuevoPrestamo);
         await getConnection().write();
         return res.json(nuevoPrestamo);
