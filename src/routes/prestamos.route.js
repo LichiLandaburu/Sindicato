fa8bde14455177ea137c558d2e11a5a8dfb8e3ce
LichiLandaburu <lichilanda@gmail.com>
2022-11-18 14:00:58 -0300

    Agregado prestamos

--- src/routes/prestamos.route.js
+++ src/routes/prestamos.route.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { createPrestamo, getPrestamos, setPagado } from "../controllers/prestamos.controller.js";
+import { createPrestamo, deletePrestamo, getPrestamos, setPagado } from "../controllers/prestamos.controller.js";
 
 const router = Router();
 
 router.post("/", createPrestamo);
 router.get("/", getPrestamos);
-router.delete("/:id_prestamo", getPrestamos)
+router.delete("/:id_prestamo", deletePrestamo)
 router.put("/pagado/:id_prestamo", setPagado);
 
 export default router;
